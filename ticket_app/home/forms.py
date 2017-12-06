d84853a3787a5ddc33444af8b9d7ed99e41ff8cc
Vineet Chawla <vineetchawla19@gmail.com>
2017-12-06 14:42:55 +0100

    Added forms for ticket editing/viewing

--- ticket_app/home/forms.py
+++ ticket_app/home/forms.py
@@ -1,35 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
-from wtforms_components import DateTimeField, read_only, DateRange
-from wtforms.fields.html5 import DateField
-from datetime import datetime
-
-from ..models import User
-
-class FlightForm(FlaskForm):
-    """
-    Forms for users to select the flights
-    """
-    flight_id = StringField('FlightID', validators=[DataRequired()])
-    departure_airport = StringField('Departure Airport', render_kw={'readonly': True})
-    departure_city = StringField('Departure City', render_kw={'readonly': True})
-    arrival_airport = StringField('Arrival Airport', render_kw={'readonly': True})
-    arrival_city = StringField('Arrival City', render_kw={'readonly': True})
-    airline = StringField('Airline', render_kw={'readonly': True})
-    arrival_time = StringField('Arrival time', render_kw={'readonly': True})
-    departure_time = DateTimeField('Departure Time', render_kw={'readonly': True})
-    date = DateField('Flight Date', validators=[DataRequired(), DateRange(min=datetime(2017,9,1))])
-    aircraft = HiddenField('Aircraft')
-    flight_duration = HiddenField('Duration')
-    airport_code = HiddenField('Airport_code')
-
-    submit = SubmitField('Calculate Insurance rates')
+from wtforms import StringField, SubmitField, SelectField, DateTimeField
+from wtforms.validators import DataRequired, Email
 
 class InsuranceForm(FlaskForm):
     """
     Form for user to select insurance
     """
     get_insurance = SubmitField('Select Insurance')
-    
 
+class CreateTicketForm(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    priority = StringField("Priority")
+    ticket_type = StringField('Type')
+    message = StringField('Massage')
+    submit = SubmitField('Submit Ticket')
+
+#Logged in users get to edit and comment on the tickets
+class EditTicketForm(CreateTicketForm):
+    Date = DateTimeField('Time created')
+    comment = StringField('Comment')
+    status = SelectField('Status', choices=[('open', 'Open'), ('in_progress', 'In Progress'),
+                                            ('completed', 'Completed'), ('rejected', 'Rejected')])
+
+class ReadTicketForm(FlaskForm):
+    first_name = StringField('First Name', render_kw={'readonly': True})
+    last_name = StringField('Last Name', render_kw={'readonly': True})
+    email = StringField('Email', render_kw={'readonly': True})
+    priority = StringField("Priority", render_kw={'readonly': True})
+    ticket_type = StringField('Type', render_kw={'readonly': True})
+    message = StringField('Massage', render_kw={'readonly': True})
